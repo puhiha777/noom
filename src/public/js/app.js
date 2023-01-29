6e7a501fc1a57ad91a74ce1a26ec18e86fea8a36
puhiha777 <111627804+puhiha777@users.noreply.github.com>
2023-01-29 20:59:56 +0900

    2.11 AdminPanel

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,18 +1,63 @@
-const socket = new WebSocketServer(`ws://${window.location.host}`);
+const socket = io();
 
-socket.addEventListener("open", ()=>{
-    console.log("Connected to Server");
-});
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
+const room = document.getElementById("room");
+room.hidden = true;
 
-socket.addEventListener("message", (message)=> {
-    console.log("Just got this: ", message.data);
-});
+let roomName;
 
-socket.addEventListener("close", () => {
-    console.log("Disconnected from Server");
+function addMessage(message) {
+  const ul = room.querySelector("ul");
+  const li = document.createElement("li");
+  li.innerText = message;
+  ul.appendChild(li);
+}
+
+function handleMessageSubmit(event) {
+  event.preventDefault();
+  const input = room.querySelector("#msg input");
+  const value = input.value;
+  socket.emit("new_message", input.value, roomName, () => {
+    addMessage(`You: ${value}`);
   });
-  
-  setTimeout(() => {
-    socket.send("hello from the browser!");
-  }, 10000);
-  
+  input.value = "";
+}
+
+function showRoom() {
+  welcome.hidden = true;
+  room.hidden = false;
+  const h3 = room.querySelector("h3");
+  h3.innerText = `Room ${roomName}`;
+  const msgForm = room.querySelector("#msg");
+  msgForm.addEventListener("submit", handleMessageSubmit);
+}
+
+function handleRoomSubmit(event) {
+  event.preventDefault();
+  const input = form.querySelector("input");
+  socket.emit("enter_room", input.value, showRoom);
+  roomName = input.value;
+  input.value = "";
+}
+
+form.addEventListener("submit", handleRoomSubmit);
+
+socket.on("welcome", (user) => {
+  addMessage(`${user} arrived!`);
+});
+
+socket.on("bye", (left) => {
+  addMessage(`${left} left.`);
+});
+
+socket.on("new_message", addMessage);
+
+socket.on("room_change", (rooms) => {
+  const roomList = welcome.querySelector("ul");
+  rooms.forEach(room => {
+    const li = document.createElement("li");
+    li.innerText = room;
+    roomList.append(li);
+  });
+});
